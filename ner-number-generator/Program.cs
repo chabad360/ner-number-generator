27f5493763f7df34654ca532b21dd674e21769ed
Mendel Greenberg <chabad360@gmail.com>
2018-01-01 03:43:17 +0000

    Update Program.cs

--- ner-number-generator/Program.cs
+++ ner-number-generator/Program.cs
@@ -17,16 +17,16 @@ namespace ner_number_generator
             }
             Console.WriteLine("Please Save the following output:");
             Console.WriteLine();
-            Console.WriteLine("#define amount " + num);
+            Console.WriteLine("#define amount " + num + " // The amount of LEDs, necessary to turn them on.");
             Console.WriteLine(" ");
-            Console.Write("int leds[" + num + "] = { 53");
+            Console.Write("/* All our LEDs (in the addressable mode) */ int leds[amount] = { 53 /* This is LED number 1 but because we use serial, we need to have it at another pin, and 53 works */");
             for (int i = 2; i <= num; i++)
             {
                 Console.Write(", " + i);
             }
             Console.WriteLine(" };");
             Console.WriteLine(" ");
-            Console.Write("String nums[" + num + "] = { \"1\"");
+            Console.Write("/* All our LEDs (the way we type them) */ String nums[amount] = { \"1\"");
             for (int i = 2; i <= num; i++)
             {
                 Console.Write(", \"" + i + "\"");
